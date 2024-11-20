fbf827f8bb1ed7dcd9818d41d81b689dd3a869b7
Maria2721 <mashaivvas@gmail.com>
2024-11-20 08:26:42 +0300

    fix: change vite config and href for deploy

--- christmas-shop/vite.config.js
+++ christmas-shop/vite.config.js
@@ -3,6 +3,13 @@ import { defineConfig } from "vite";
 export default defineConfig({
 	build: {
 		sourcemap: true,
+		rollupOptions: {
+			input: {
+				index: "index.html",
+				gifts: "src/pages/gifts.html",
+				contact: "src/pages/contacts.html",
+			},
+		},
 	},
 	base: "/maria2721-JSFE2024Q4/christmas-shop/",
 });
